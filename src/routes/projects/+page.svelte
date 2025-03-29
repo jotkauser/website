3c960cf69bec5ec1016232f5ea8f665d1f1dff85
motylek <realmotylek@gmail.com>
2025-03-30 00:32:08 +0100

    add projects

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,7 +1,19 @@
 <script lang="ts">
 	import { title } from '$lib/titleStore';
     import NavBar from '../../components/+NavBar.svelte';
+	import { projects } from '../../strings';
 	title.set('Projects');
 </script>
 
 <NavBar currentPage="projects" />
+
+<div class="flex flex-col justify-center bg-card p-4 md:p-10 rounded-xl mx-5 sm:mx-auto max-w-3xl md:w-170 gap-2">
+	<header>projects</header>
+	<ul>
+		{#each projects as project}
+			<li class="mb-3"><a href={project.link} target="_blank" rel="noopener noreferrer">{project.name} {#if project.description} - {project.description}{/if}</a></li>
+		{/each}
+	</ul>
+
+
+</div>
--- src/strings.ts
+++ src/strings.ts
@@ -6,3 +6,41 @@ export const socials = {
 };
 
 export const gitRepo = 'https://git.motylek.ovh';
+
+export const projects = [
+	{
+		name: "this website",
+		link: "https://github.com/jotkauser/website",
+		description: "this website that you are looking at right now"
+	},
+	{
+		name: "hebece.js",
+		link: "https://github.com/hypedevss/hebece",
+		description: "library for interacting with eduvulcan's mobile api made in node.js"
+	},
+	{
+		name: "vulcanjs",
+		link: "https://github.com/hypedevss/vulcanjs",
+		description: "experimental and unfinished eduvulcan client that runs in a terminal"
+	},
+	{
+		name: "veribucore",
+		link: "https://github.com/hypedevss/veribucore",
+		description: "low quality verification bot that was made for a specific server"
+	},
+	{
+		name: "nglspam",
+		link: "https://github.com/jotkauser/NGLspam",
+		description: "easy to use tool that allows spamming a ngl.link inbox"
+	},
+	{
+		name: "nope.db-nl",
+		link: "https://github.com/jotkauser/nope.db-nl",
+		description: "modded nope.db, currently no longer in active development"
+	},
+	{
+		name: "meowcdn",
+		link: "https://github.com/meowcdn",
+		description: "self-hostable cdn frontend made in php, not finished and public, yet"
+	}
+]
