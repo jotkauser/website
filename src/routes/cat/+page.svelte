acca829b9d934c1396ecd978e2fac9149e786eb9
motylek <realmotylek@gmail.com>
2025-04-11 22:21:20 +0200

    add a cat photo

--- src/routes/cat/+page.svelte
+++ src/routes/cat/+page.svelte
@@ -8,5 +8,5 @@
 
 <div class="flex flex-col justify-center bg-card p-4 md:p-10 rounded-xl mx-5 sm:mx-auto max-w-3xl md:w-170 gap-2">
 	<header>meow</header>
-	<p>unfortunately no photos yet. need to take a good one</p>
+	<img src="/assets/cat/cat1.jpg" alt="cat" class="w-full rounded-xl">
 </div>
